3654dbe19657dbf204b13284d51bfbade507ef74
Cuahutli Miguel Ulloa Robles <ing.cuahutliulloa@gmail.com>
2018-04-20 16:15:34 -0600

    implementando su funcionalidad y  testeando el reseteo de password

--- src/djboards/settings.py
+++ src/djboards/settings.py
@@ -130,5 +130,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+#CONFIGS TO THIS PROJECT
 LOGOUT_REDIRECT_URL = 'home'
 LOGIN_REDIRECT_URL = 'home'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
